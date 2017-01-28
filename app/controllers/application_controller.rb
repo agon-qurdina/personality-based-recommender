9bb13865cba92d7a1174f6085f00b5929a8a0fa9
agonq <agon.qurdina@gmail.com>
2017-01-29 00:35:52 +0100

    User info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def test
-    extremes = Facebook.get_extremes
+    personality1 = Personality.where({id: 28}).first
+    personality2 = Personality.where({id: 29}).first
+    distance = Product.new.eucledian_distance(personality1, personality2)
 
-    render json: extremes
+    personality1 = Personality.where({id: 29}).first
+    personality2 = Personality.where({id: 30}).first
+    distance2 = Product.new.eucledian_distance(personality1, personality2)
+
+    personality1 = Personality.where({id: 30}).first
+    personality2 = Personality.where({id: 32}).first
+    distance3 = Product.new.eucledian_distance(personality1, personality2)
+
+    render json: {distance: distance, distance2: distance2, distance3: distance3}
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 
 class Product < ApplicationRecord
+
+  def eucledian_distance(personality1, personality2)
+    Math.sqrt(
+            ((personality1.extraversion - personality2.extraversion)**2) +
+            ((personality1.agreeableness - personality2.agreeableness)**2) +
+            ((personality1.conscientiousness- personality2.conscientiousness)**2) +
+            ((personality1.neuroticism- personality2.neuroticism)**2)
+    ).to_f.round(3)
+  end
 end
+0
