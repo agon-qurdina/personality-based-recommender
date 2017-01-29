bfb1521c1e690f4c7d38ccf815e319b5043a62b8
agonq <agon.qurdina@gmail.com>
2017-01-29 18:18:48 +0100

    User info

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,14 @@
 class Product < ApplicationRecord
 
   def get_closest_products
-    products = Product.where('id != ?',self[:id]).select('id,title,image,extraversion,agreeableness,conscientiousness,neuroticism,openness')
+    products = Product.where('id != ?',self[:id]).to_a
     product_distances = {}
     products.each { |product| product_distances[product.id] = Personality.eucledian_distance self,product }
     product_distances = product_distances.sort_by { |k,v| v }.first(5).to_h
     keys = product_distances.keys
     final_products = []
     keys.each do |key|
-      final_products.push products.where({id: key}).first
+      final_products.push products.select { |p| p.id == key}.first
     end
     final_products
   end
